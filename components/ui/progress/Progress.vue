f35f2c7ba8d2a06eb43fa499b3b1ccb14a2ac09e
Kwaku Okyere Adusei <32445365+asteroid2k@users.noreply.github.com>
2024-12-06 23:53:34 +0000

    added adsense

--- components/ui/progress/Progress.vue
+++ components/ui/progress/Progress.vue
@@ -8,7 +8,7 @@ import {
 import { computed, type HTMLAttributes } from 'vue'
 
 const props = withDefaults(
-  defineProps<ProgressRootProps & { class?: HTMLAttributes['class'] }>(),
+  defineProps<ProgressRootProps & { class?: HTMLAttributes['class'], indicatorClass?: string }>(),
   {
     modelValue: 0,
   },
@@ -22,18 +22,14 @@ const delegatedProps = computed(() => {
 </script>
 
 <template>
-  <ProgressRoot
-    v-bind="delegatedProps"
-    :class="
-      cn(
-        'relative h-2 w-full overflow-hidden rounded-full bg-primary/20',
-        props.class,
-      )
-    "
-  >
-    <ProgressIndicator
-      class="h-full w-full flex-1 bg-primary transition-all"
-      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
-    />
+  <ProgressRoot v-bind="delegatedProps" :class="cn(
+    'relative h-2 w-full overflow-hidden rounded-full bg-primary/20',
+    props.class,
+  )
+    ">
+    <ProgressIndicator :class="cn(
+      'h-full w-full flex-1 transition-all',
+      props.indicatorClass,
+    )" :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`" />
   </ProgressRoot>
 </template>
