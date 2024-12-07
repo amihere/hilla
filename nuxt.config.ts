6cc224224b71628944782c407f42503ed2d08568
Kwaku Okyere Adusei <32445365+asteroid2k@users.noreply.github.com>
2024-12-07 16:39:21 +0000

    fix results query WIP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
 		head: {
 			charset: "utf-8",
 			viewport: "width=device-width, initial-scale=1",
-			title: "Hilla",
+			title: "Ghana Elections Results",
 			meta: [],
 		},
 	},
